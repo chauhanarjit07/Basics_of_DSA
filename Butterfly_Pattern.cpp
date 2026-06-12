ccafa1a8276a79d7f020e8e1bb22e615e2a70d8c
Arjit chauhan <chauhanarjit07@gmail.com>
2026-06-13 02:13:47 +0530

    Create Butterfly_Pattern.cpp

---
+++ Butterfly_Pattern.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int i,j,n,k;
+  cout<<"Enter rows: ";
+  cin>> n;
+  for(i=0; i<n; i++){
+    for(j=0; j<=i;j++){
+      cout<<"*";
+    }
+    int x=(2*n)-2-(2*i);
+    for(k=0; k<x;k++){
+    cout<<" ";
+  }
+  for(j=0; j<=i;j++){
+    cout<<"*";
+  }
+    cout<<endl;
+  }
+  for(i=1; i<n;i++){
+    for(j=0; j<n-i; j++){
+      cout<<"*";
+    }
+    for(k=0; k<2*i; k++){
+      cout<<" ";
+    }
+    for(j=0; j<n-i; j++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+  
+  return 0;
+}
