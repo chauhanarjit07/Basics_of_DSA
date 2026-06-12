99f592153b042fc7473acdaccf6da429c1c21d3d
Arjit chauhan <chauhanarjit07@gmail.com>
2026-06-12 13:29:18 +0530

    Create Star_Hill_Pattern.cpp

---
+++ Star_Hill_Pattern.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int i,j,n;
+  cout<<"Enter rows: ";
+    cin>> n;
+  for(i=0; i<n; i++){
+    for(j=0; j<=i; j++){
+        cout<<"*";
+    }
+    cout<<endl;
+  }
+  for(i=0; i<n; i++){
+    for(j=0; j<n-i-1; j++){
+        cout<<"*";
+    }
+    cout<<endl;
+  }
+  return 0;
+}
