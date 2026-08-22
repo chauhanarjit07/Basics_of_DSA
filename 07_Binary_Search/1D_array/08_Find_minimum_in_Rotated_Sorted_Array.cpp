a49e6f567bc0f07f6b89729abef69f70ec894b4c
Arjit chauhan <chauhanarjit07@gmail.com>
2026-08-22 18:50:50 +0530

    Add files via upload

---
+++ 07_Binary_Search/1D_array/08_Find_minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int findMin(vector<int>& nums) {
+    int low=0, n=nums.size();
+    int high= n-1;
+    int ans= INT_MAX;
+    while(low<=high){
+        int mid= (low+high)/2;
+        if(nums[low]<= nums[high]){
+            ans= min(ans,nums[low]);
+            break;
+        }
+        if(nums[low]<=nums[mid]){
+            ans= min(ans,nums[low]);
+            low= mid+1;
+        }
+        else{
+            ans= min(ans,nums[mid]);
+            high= mid-1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int n,i;
+    cout<< "Enter N: ";
+    cin>>n;
+    vector<int> nums(n);
+    for(i=0; i<n; i++){
+        cin>>nums[i];
+    }
+    findMin(nums);
+}
