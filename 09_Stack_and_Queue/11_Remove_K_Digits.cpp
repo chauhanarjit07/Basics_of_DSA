059a4c628ac27ab543aed36b9e812869cf424b04
Arjit chauhan <chauhanarjit07@gmail.com>
2026-09-24 22:33:04 +0530

    Add files via upload

---
+++ 09_Stack_and_Queue/11_Remove_K_Digits.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> st;
+        for(int i=0; i<num.size(); i++){
+            while(!st.empty() && k && st.top()> num[i] ){
+                st.pop();
+                k--;
+            }
+            st.push(num[i]);
+            
+        }
+        while(!st.empty() && k){
+            st.pop();
+            k--;
+        }
+        if(st.empty()) return "0";
+        string res="";
+        while(!st.empty()){
+            res+= st.top();
+            st.pop();
+        }
+        while(!res.empty() && res.back()=='0') res.pop_back();
+        reverse(res.begin(), res.end());
+        if(res.empty()) return "0";
+        return res;
+    }
+};
